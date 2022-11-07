c29b5995935986d58d19a432eb2628568ac05a06
OmarMahmoudElsherif <104269847+OmarMahmoudElsherif@users.noreply.github.com>
2022-11-08 00:44:22 +0200

    Add files via upload

---
+++ Eclipse_wk/Debug/Fan_Controller_system.d
@@ -0,0 +1,17 @@
+Fan_Controller_system.o Fan_Controller_system.o:  \
+ ../Fan_Controller_system.c ../adc.h ../std_types.h ../lcd.h \
+  ../lm35_sensor.h ../PWM_Timer0.h ../dc_motor.h ../gpio.h
+
+../adc.h:
+
+../std_types.h:
+
+../lcd.h:
+
+../lm35_sensor.h:
+
+../PWM_Timer0.h:
+
+../dc_motor.h:
+
+../gpio.h:
---
+++ Eclipse_wk/Debug/PWM_Timer0.d
@@ -0,0 +1,5 @@
+PWM_Timer0.o PWM_Timer0.o: ../PWM_Timer0.c ../PWM_Timer0.h ../std_types.h
+
+../PWM_Timer0.h:
+
+../std_types.h:
